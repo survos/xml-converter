dd67badcf252cbbb7714dd4c33c06d5ad3122f43
Tac Tacelosky <tacman@gmail.com>
2022-08-25 12:58:13 -0400

    add router

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
+const { decodeParams, decodeExposureParams, encodeExposureParams } = require('@wmakeev/darkroom-xmp-tools')
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World! <a href="/convert/exposure/?p=0000000040a0093bd8ce374000004842000080c0">Test</a>')
+})
+
+app.get('/convert/:type/', function(req, res, next) {
+    // res.send(req.query.p);
+    let paramsObj = decodeParams(req.params.type, req.query.p);
+    res.send(paramsObj);
+    // res.send('user ' + req.user.name);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
 var http = require('http');
 const url = require('url');
 const assert = require('assert')
-const { decodeExposureParams, encodeExposureParams } = require('@wmakeev/darkroom-xmp-tools')
 
 //create a server object:
+if (0)
 http.createServer(function (req, res) {
-        const queryObject = url.parse(req.url,true).query;
+    const queryObject = url.parse(req.url,true).query;
 
-        let paramStr = queryObject.params;
+    let paramStr = queryObject.params;
     console.log(paramStr, queryObject());
 
     const EXPOSURE_PARAMS_BIN_STR = '0000000040a0093bd8ce374000004842000080c0';
